f9a58d91935d56ff6d9c273702bb0437585cd2e5
Andre Heringer <andregomesh@dcc.ufmg.br>
2020-09-05 21:24:47 -0300

    Do operations in place

--- scripts/remove_columns.py
+++ scripts/remove_columns.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv('../data/yellow_tripdata_2019-04-01.csv')
+df = pd.read_csv('data/yellow_tripdata_2019-04.csv')
 
 # Comment out the collumns you want to keep
 df.drop('VendorID', axis=1, inplace=True)
 #df.drop('tpep_pickup_datetime', axis=1, inplace=True)
-#df.drop('tpep_dropoff_datetime', axis=1, inplace=True)
+df.drop('tpep_dropoff_datetime', axis=1, inplace=True)
 #df.drop('passenger_count', axis=1, inplace=True)
 df.drop('trip_distance', axis=1, inplace=True)
 df.drop('RatecodeID', axis=1, inplace=True)
@@ -23,6 +23,7 @@ df.drop('total_amount', axis=1, inplace=True)
 df.drop('congestion_surcharge', axis=1, inplace=True)
 
 df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+df.drop_duplicates(subset=['tpep_pickup_datetime'], keep='first', inplace=True)
 df.sort_values(by='tpep_pickup_datetime', inplace=True)
-
-df.to_csv('parsed_sample.csv', index = False)
+df.rename(columns={'tpep_pickup_datetime': 'timestamp', 'passenger_count': 'value'}, inplace=True)
+df.to_csv('yellow_tripdata_2019-04_parsed.csv', index = False)
