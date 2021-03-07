5e384624b5c60443811d5039b23539d3f12b6089
Andre Heringer <andregomesh@dcc.ufmg.br>
2021-03-07 19:21:55 -0300

    Refatore log event

--- src/stream.rs
+++ src/stream.rs
@@ -1,18 +1,77 @@
+extern crate chrono;
+extern crate fasthash;
+extern crate serde;
 extern crate serde_json;
 
 use crate::errors::RstzError;
-use crate::log_event::LogEvent;
 
-use std::{fs::File, io::BufReader, path::Path};
+use chrono::{DateTime, Datelike, Timelike, Utc};
+use fasthash::city::hash64;
+use serde::{Serialize, Deserialize};
+use serde_json::Value;
+use std::collections::BTreeMap;
+use serde_json::ser::to_string;
+
+use std::fmt;
+use std::{fs::File, io::BufReader};
+
+///Most basic implementation of a Log Event, contains the same caracteristics defined by vector.
+///Timestamp and host fields are requierd.
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct LogEvent {
+  timestamp: DateTime<Utc>,
+  host: String,
+
+  #[serde(flatten)]
+  values: BTreeMap<String, Value>,
+}
+
+impl LogEvent {
+  pub fn new(timestamp: DateTime<Utc>, host: String, values: BTreeMap<String, Value>) -> Self {
+    LogEvent {
+      timestamp,
+      host,
+      values,
+    }
+  }
+
+  pub fn serieskey(&self) -> [u8; 16] {
+    let mut res: [u8; 16] = [0; 16];
+    res[..8].copy_from_slice(&hash64(self.host.as_bytes()).to_ne_bytes());
+    let year: [u8; 4] = self.timestamp.naive_local().date().year().to_ne_bytes();
+    res[8..12].copy_from_slice(&year);
+    let md = [
+      self.timestamp.naive_local().date().month() as u8,
+      self.timestamp.naive_local().date().day() as u8,
+    ];
+    res[12..14].copy_from_slice(&md);
+    let hms = [
+      self.timestamp.naive_local().time().hour() as u8,
+      self.timestamp.naive_local().time().minute() as u8,
+    ];
+    res[14..].copy_from_slice(&hms);
+    res
+  }
+
+  pub fn to_string(&self) -> String {
+    to_string(self).unwrap_or_default()
+  }
 
 
-pub fn read_log_events_from_file<P: AsRef<Path>>(path: P) -> Result<LogEvent, RstzError> {
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
+}
 
-    // Read the JSON contents of the file as an instance of `User`.
-    let u = serde_json::from_reader(reader).map_err(|e| RstzError::Message(e.to_string()))?;
+impl fmt::Display for LogEvent {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "({}:{} {:?})", self.host, self.timestamp, self.values)
+  }
+}
 
-    // Return the `User`.
-    Ok(u)
+
+pub fn stream_from_file<'fs>(
+  file: &'fs File,
+) -> serde_json::StreamDeserializer<'fs, serde_json::de::IoRead<BufReader<&File>>, LogEvent>
+{
+  let reader = BufReader::new(file);
+  let deserializer = serde_json::Deserializer::from_reader(reader);
+  deserializer.into_iter::<LogEvent>()
 }
